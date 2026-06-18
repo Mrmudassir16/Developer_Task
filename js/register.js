9e90536e5626d2eb316d6cc58505632a3b62698f
Mohammed Mudassir <abdulbasithmudassir@gmail.com>
2026-06-19 03:08:44 +0530

    Update icons to inline SVGs and remove dump.rdb from tracking

--- js/register.js
+++ js/register.js
@@ -5,15 +5,19 @@ $(document).ready(function () {
     const $loader = $('#loader-overlay');
 
     function showAlert(message, isSuccess = false) {
+        const warningSvg = `<svg class="icon-svg" style="color: #fca5a5;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="18" height="18"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg>`;
+        const successSvg = `<svg class="icon-svg" style="color: #a7f3d0;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="18" height="18"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>`;
+        
         $alertBox.removeClass('alert-success');
         $alertBox.css('display', 'none');
         $alertMsg.text(message);
         
+        const $iconContainer = $('#alert-icon-container');
         if (isSuccess) {
             $alertBox.addClass('alert-success');
-            $alertBox.find('i').attr('class', 'fa-solid fa-circle-check me-2');
+            $iconContainer.html(successSvg);
         } else {
-            $alertBox.find('i').attr('class', 'fa-solid fa-circle-exclamation me-2');
+            $iconContainer.html(warningSvg);
         }
         
         $alertBox.css('display', 'flex').hide().fadeIn(300);
